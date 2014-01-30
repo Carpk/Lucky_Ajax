58bbad7fe7d46690793f4d4c3386b881ea19a878
Promoveo <zappeln@gmail.com>
2014-01-29 20:23:52 -0600

    working ajax

--- public/js/application.js
+++ public/js/application.js
@@ -6,9 +6,10 @@ $(document).ready(function () {
   //   3- generate a random number between 1 and 6 using JavaScript
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
+
   $('form').click(function(event) {
     event.preventDefault()
     var random = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-
-  })
+    $.post('/rolls',).html(random);
+  });
 });
